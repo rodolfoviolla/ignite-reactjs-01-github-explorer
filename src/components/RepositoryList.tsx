6842ac61bfbc3277a83cb4f8b5ee890409fd9b43
Rodolfo Della Violla <rodolfo.violla@eduzz.com>
2021-05-25 21:49:05 -0300

    Finaliza modulo 1

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-import { RepositoryItem } from "./RepositoryItem";
+import { Repository, RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
